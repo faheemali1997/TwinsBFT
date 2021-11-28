34b9205d0c71b9e9b0fd2c0af0bb414d7c8d7a78
Akash <akashsateesh.417@gmail.com>
2021-11-28 14:42:09 -0500

    fix: generator code

--- generator/generator.py
+++ generator/generator.py
@@ -1,14 +1,18 @@
+import random
+
 from stringling import stirling2
 from itertools import product
 from json import dumps
 from collections import defaultdict
+from itertools import combinations
+from iteration_utilities import random_combination
 from os.path import join
 import os
 
 
 class TestGenerator:
 
-    def __init__(self, nodes: list, rounds: int):
+    def __init__(self, nodes: list, rounds: int, limit : int, randomness: bool = True):
         """
         0 1 2 3 4.....n -> Nodes
         0 1 2 3 4.....f -> byzentine nodes
@@ -25,6 +29,18 @@ class TestGenerator:
         self.number_of_byzentine_nodes = (len(nodes) - 1) // 3
         self.byzentine_nodes = [w for w in range(self.number_of_byzentine_nodes)]
         self.nodes = [w for w in range(len(self.total_nodes) + len(self.byzentine_nodes))]
+        self.limit = limit
+        self.twins = self.generate_twins(self.byzentine_nodes)
+        self.randomness = randomness
+
+    def generate_twins(self, byzentine_nodes: list):
+
+        twins = defaultdict(list)
+
+        for node in byzentine_nodes:
+            twins[node] = self.get_twin(node)
+
+        return twins
 
     def get_twin(self, node):
 
@@ -32,8 +48,28 @@ class TestGenerator:
         return self.nodes[len(self.total_nodes) - 1 + node]
 
     def make_partitions(self, limit: int, partitions: int):
-        
-        return stirling2(len(self.total_nodes), min(limit, partitions))
+
+        return stirling2(len(self.nodes), min(limit, partitions))
+
+    def combine_leader_partition_random(self, partitions):
+
+        all_leaders = self.byzentine_nodes[::]
+
+        size = len(partitions)
+
+        while size > 0:
+
+            random_partition = random.randint(0, size - 1)
+
+            partition = partitions[random_partition]
+
+            leader = random.randint(0, len(all_leaders) - 1)
+
+            yield leader, partition
+
+            size -= 1
+
+            partitions.pop(random_partition)
 
     def combine_partitions_leaders(self, partitions):
 
@@ -46,16 +82,34 @@ class TestGenerator:
         all_leaders = self.byzentine_nodes[::]
 
         for partition in partitions:
+
             for leader in all_leaders:
-                yield (leader, partition)
 
-    def combine_scenarios_with_rounds(self, cases):
+                yield leader, partition
 
-        return product(cases, repeat=self.rounds)
+    def combine_scenarios_with_rounds(self, cases, limit: int):
+
+        # select random combinations of leader, round and partition
+        return random_combination(combinations(cases, r=self.rounds), r=limit)
 
     @staticmethod
     def empty_list():
         return []
+
+    @staticmethod
+    def select_random_nodes_within_partition(partition: list):
+
+        result = []
+
+        idx = random.randint(0, len(partition) - 1)
+
+        if len(partition[idx]) < 2:
+            return []
+
+        # select two nodes within random partition and drop the messages among them
+        li = random.sample(range(0, len(partition) - 1), 2)
+
+        return li
 
     def get_json_dump(self, testcases):
 
@@ -68,11 +122,12 @@ class TestGenerator:
              
         """
 
-        
         for testcase in testcases:
 
             round_leaders, round_partitions = defaultdict(TestGenerator.empty_list), defaultdict(
                 TestGenerator.empty_list)
+
+            random_message_drops = defaultdict(list)
 
             for round_number, partition in enumerate(testcase):
 
@@ -91,19 +146,28 @@ class TestGenerator:
 
                 round_partitions[round_number+1].append(round_partition)
 
+                message_drop = random.randint(0, 1)
+
+                # Generate random message drops
+                message_drops_nodes = TestGenerator.select_random_nodes_within_partition(round_partition)
+
+                if message_drop:
+                    random_message_drops[round_number + 1].append(message_drops_nodes)
+
             json_dump.append([{
                 'round_leaders': round_leaders,
-                'partitions': round_partitions
+                'partitions': round_partitions,
+                'random_message_drops': random_message_drops
             }])
 
         return dumps(
             {
                 'number_of_nodes': len(self.total_nodes),
                 'number_of_twins': len(self.byzentine_nodes),
-                'test_case_scenarios': json_dump
+                'test_case_scenarios': json_dump,
+                'twins': self.twins
             },
-
-            indent=4
+            indent=3
         )
 
     def write_to_file(self, json_dump, file_name):
@@ -113,19 +177,29 @@ class TestGenerator:
 
         print("Writing Done, Thank You")
 
-
     def run(self, offline: bool):
 
+        # make partitions make_partitions(limit, number_of_partitions)
         partitions = self.make_partitions(4, 3)
 
-        scenarios = self.combine_partitions_leaders(partitions)
+        # Generate leader partition pairs randomly
+        if self.randomness:
+            scenarios = self.combine_leader_partition_random(partitions)
 
-        testcases = self.combine_scenarios_with_rounds(scenarios)
+        # Generate leader partition pairs in order based on user input
+        else:
+            scenarios = self.combine_partitions_leaders(partitions)
 
-        limit = 10
+        # Limit generated during constructor, if it's None, it is defaulted to 10
+        limit = 10 if self.limit is None else self.limit
 
+        # Generate leader_partition_pairs with rounds
+        testcases = self.combine_scenarios_with_rounds(scenarios, limit)
+
+        # Create a new array for holding testcases
         testcases_new = []
 
+        # Generate all test_cases
         for testcase in testcases:
 
             if limit == 0:
@@ -135,18 +209,20 @@ class TestGenerator:
 
             testcases_new.append(testcase)
 
+        # Create json dump for dumping to file
         json_dump = self.get_json_dump(testcases_new)
 
+        # Offline means writing to file
         if not offline:
             return json_dump
 
+        # Write to file so that executor reads it from there
         self.write_to_file(json_dump, 'testcase_1.json')
-
 
 
 if __name__ == '__main__':
 
-    gen = TestGenerator([0, 1, 2, 3, 4, 5, 6], 2)
+    gen = TestGenerator([0, 1, 2, 3, 4, 5, 6], 2, None)
 
     gen.run(True)
 
